3dc502fb423ba422607d29f0c4ac13e94cba326b
Shuhei Kayawari <dev.wowry@gmail.com>
2022-01-07 19:40:16 +0900

    Update Description

--- src/components/Description/index.tsx
+++ src/components/Description/index.tsx
@@ -10,27 +10,33 @@ const styles = {
   container: css`
     width: 90%;
     margin-top: 20px;
+
+    h1 {
+      margin: 0;
+    }
   `,
 };
 
 const Description: React.VFC = () => {
   return (
     <div css={styles.container}>
-      <Typography>
-        スクリーンショットなどの画像からQRコードを検出するアプリです。
-      </Typography>
-      <List>
-        <ListItem disablePadding>
-          <Alert severity="success" sx={{ backgroundColor: "#fff" }}>
-            画像のコピー&ペースト、ドラッグ&ドロップに対応
-          </Alert>
-        </ListItem>
-        <ListItem disablePadding>
-          <Alert severity="success" sx={{ backgroundColor: "#fff" }}>
-            複数のQRコード検出に対応
-          </Alert>
-        </ListItem>
-      </List>
+      <h1>
+        <Typography>
+          スクリーンショットなどの画像からQRコードを検出するアプリです。
+        </Typography>
+        <List>
+          <ListItem disablePadding>
+            <Alert severity="success" sx={{ backgroundColor: "#fff" }}>
+              画像のコピー&ペースト、ドラッグ&ドロップに対応
+            </Alert>
+          </ListItem>
+          <ListItem disablePadding>
+            <Alert severity="success" sx={{ backgroundColor: "#fff" }}>
+              複数のQRコード検出に対応
+            </Alert>
+          </ListItem>
+        </List>
+      </h1>
     </div>
   );
 };
