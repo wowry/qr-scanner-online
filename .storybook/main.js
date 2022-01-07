8ee6b97cac07cdafbefa1320f6cb2ebf904b85a0
Shuhei Kayawari <dev.wowry@gmail.com>
2022-01-08 03:44:21 +0900

    Update storybook config

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,9 +1,32 @@
 module.exports = {
   typescript: { reactDocgen: false },
-  stories: ["../src/**/*.stories.@(js|jsx|ts|tsx|mdx)"],
-  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/preset-scss",
+  ],
   framework: "@storybook/react",
   core: {
     builder: "webpack5",
+  },
+  webpackFinal: async (baseConfig) => {
+    baseConfig.module.rules.push({
+      test: /\.scss$/,
+      use: [
+        "style-loader",
+        {
+          loader: "css-loader",
+          options: {
+            importLoaders: 1, // postcss-loader
+            modules: {
+              localIdentName: "[local]___[hash:base64:2]",
+            },
+          },
+        },
+        "sass-loader",
+      ],
+    });
+    return { ...baseConfig };
   },
 };
