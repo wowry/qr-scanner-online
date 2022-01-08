0b08b1f69958ba55b3c04c0185209069092fe8fa
Shuhei Kayawari <dev.wowry@gmail.com>
2022-01-08 18:41:37 +0900

    Add CookieConsentBar

---
+++ src/components/CookieConsentBar/index.tsx
@@ -0,0 +1,55 @@
+/** @jsxImportSource @emotion/react */
+import React from "react";
+import CookieConsent from "react-cookie-consent";
+import { red } from "@mui/material/colors";
+import SecurityIcon from "@mui/icons-material/Security";
+
+const styles = {
+  container: {
+    opacity: 0.9,
+  },
+  content: {
+    display: "inline-flex",
+    alignItems: "center",
+  },
+  button: {
+    display: "inline-flex",
+    borderRadius: "4px",
+    margin: 15,
+    padding: "0.5rem 1rem",
+    flex: "0 0 auto",
+    cursor: "pointer",
+  },
+  approve: {
+    backgroundColor: red[500],
+    border: `1px solid ${red[500]}`,
+    color: "#fff",
+  },
+  decline: {
+    backgroundColor: "#fff",
+    border: `1px solid ${red[500]}`,
+    color: red[500],
+  },
+};
+
+const CookieConsentBar: React.VFC = () => {
+  return (
+    <CookieConsent
+      buttonText="同意する"
+      declineButtonText="拒否する"
+      enableDeclineButton
+      expires={150}
+      disableButtonStyles
+      style={styles.container}
+      contentStyle={styles.content}
+      buttonStyle={{ ...styles.button, ...styles.approve }}
+      declineButtonStyle={{ ...styles.button, ...styles.decline }}
+      onAccept={() => window.consentGranted()}
+    >
+      <SecurityIcon style={{ marginRight: "0.5rem" }} />
+      当サイトは利便性向上のため、Cookieを使用します。
+    </CookieConsent>
+  );
+};
+
+export default CookieConsentBar;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,42 @@
-import "../styles/globals.scss";
+import { useEffect } from "react";
 import type { AppProps } from "next/app";
+import { useRouter } from "next/router";
+import GAScripts from "../components/GAScripts";
+import CookieConsentBar from "../components/CookieConsentBar";
 import { Provider } from "react-redux";
 import { store } from "../libs/store";
+import * as gtag from "../libs/gtag";
+import "../styles/globals.scss";
+
+declare global {
+  // eslint-disable-next-line no-unused-vars
+  interface Window {
+    consentGranted: any;
+  }
+}
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouteChange = (url: string) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
-    <Provider store={store}>
-      <Component {...pageProps} />
-    </Provider>
+    <>
+      <GAScripts />
+
+      <Provider store={store}>
+        <Component {...pageProps} />
+      </Provider>
+
+      <CookieConsentBar />
+    </>
   );
 }
 
