dd9225b5ed6ee6c6d965909a58c08ab61b23c022
Shuhei Kayawari <dev.wowry@gmail.com>
2022-01-13 01:30:53 +0900

    Update Description

--- src/components/Description/index.tsx
+++ src/components/Description/index.tsx
@@ -1,7 +1,6 @@
 /** @jsxImportSource @emotion/react */
 import React from "react";
 import { css } from "@emotion/react";
-import Typography from "@mui/material/Typography";
 import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
 import Alert from "@mui/material/Alert";
@@ -10,33 +9,33 @@ const styles = {
   container: css`
     width: 90%;
     margin-top: 20px;
-
-    h1 {
-      margin: 0;
-    }
+  `,
+  description: css`
+    margin: 0;
+    font-size: 1rem;
   `,
 };
 
 const Description: React.VFC = () => {
   return (
     <div css={styles.container}>
-      <h1>
-        <Typography>
-          スクリーンショットなどの画像からQRコードを検出するアプリです。
-        </Typography>
-        <List>
-          <ListItem disablePadding>
-            <Alert severity="success" sx={{ backgroundColor: "#fff" }}>
-              画像のコピー&ペースト、ドラッグ&ドロップに対応
-            </Alert>
-          </ListItem>
-          <ListItem disablePadding>
-            <Alert severity="success" sx={{ backgroundColor: "#fff" }}>
-              複数のQRコード検出に対応
-            </Alert>
-          </ListItem>
-        </List>
+      <h1 css={styles.description}>
+        スクリーンショットなどの画像からQRコードを検出するアプリです。
       </h1>
+      <List>
+        <ListItem disablePadding>
+          <Alert severity="success" sx={{ backgroundColor: "#fff" }}>
+            <h2 css={styles.description}>
+              画像のコピー&ペースト、ドラッグ&ドロップに対応
+            </h2>
+          </Alert>
+        </ListItem>
+        <ListItem disablePadding>
+          <Alert severity="success" sx={{ backgroundColor: "#fff" }}>
+            <h2 css={styles.description}>複数のQRコード検出に対応</h2>
+          </Alert>
+        </ListItem>
+      </List>
     </div>
   );
 };
