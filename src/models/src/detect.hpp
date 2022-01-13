ae9085e7748bf3b13457ec3f2bbc51c755885571
Shuhei Kayawari <dev.wowry@gmail.com>
2022-01-14 01:12:12 +0900

    Add comment to detect.hpp

--- src/models/src/detect.hpp
+++ src/models/src/detect.hpp
@@ -7,12 +7,32 @@
 class Detect
 {
 public:
+  /**
+   * 複数のQRコードを同時検出する
+   * @param addr 入力画像のアドレス
+   * @param width 入力画像の幅
+   * @param height 入力画像の高さ
+   * @return QRコードが検出されたかどうか
+   */
   bool detect(std::size_t addr, int width, int height);
+
+  /**
+   * 検出されたQRコードの座標を得る
+   * @return 検出されたQRコードの座標
+   */
   std::vector<float> getPoints();
+
+  /**
+   * 検出されたQRコードのデコード結果（URL）を得る
+   * @return 検出されたQRコードのデコード結果（URL）
+   */
   std::vector<std::string> getDecodedInfo();
 
 private:
+  //! QRコードの座標
   cv::Mat_<cv::Vec2f> _points;
+
+  //! QRコードのデコード結果（URL）
   std::vector<std::string> _decoded_info;
 };
 
