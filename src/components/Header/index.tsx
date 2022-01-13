7c7c0226e143caa6dffa5a3f15849078f98357ed
Shuhei Kayawari <dev.wowry@gmail.com>
2022-01-14 01:24:19 +0900

    Update Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,13 @@
 /** @jsxImportSource @emotion/react */
 import React from "react";
+import Link from "next/link";
 import { css } from "@emotion/react";
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import GitHub from "@mui/icons-material/GitHub";
+import PolicyIcon from "@mui/icons-material/Policy";
+import Tooltip from "@mui/material/Tooltip";
 
 const styles = {
   container: css`
@@ -12,8 +15,19 @@ const styles = {
   `,
   title: css`
     flex-grow: 1;
-    margin-left: 12px;
-    font-size: 1.2rem;
+
+    a {
+      display: inline-flex;
+    }
+
+    p {
+      margin-left: 12px;
+      font-size: 1.2rem;
+    }
+  `,
+  icons: css`
+    margin-left: 20px;
+    cursor: pointer;
   `,
 };
 
@@ -21,15 +35,31 @@ const Header: React.VFC = () => {
   return (
     <AppBar position="relative">
       <Toolbar css={styles.container}>
-        <img src="/favicon.ico" alt="logo" width={28} height={28} />
-        <Typography css={styles.title}>QR Scanner Online</Typography>
-        <div>
+        <div css={styles.title}>
+          <Link href="/" passHref>
+            <a style={{ display: "inline-flex", alignItems: "center" }}>
+              <img src="/favicon.ico" alt="logo" width={28} height={28} />
+              <Typography>QR Scanner Online</Typography>
+            </a>
+          </Link>
+        </div>
+        <div style={{ display: "inline-flex", justifyContent: "flex-end" }}>
+          <Link href="/policy">
+            <a css={styles.icons}>
+              <Tooltip title="プライバシーポリシー">
+                <PolicyIcon />
+              </Tooltip>
+            </a>
+          </Link>
           <a
+            css={styles.icons}
             href="https://github.com/wowry/qr-scanner-online"
             target="_blank"
             rel="noopener noreferrer"
           >
-            <GitHub />
+            <Tooltip title="GitHub">
+              <GitHub />
+            </Tooltip>
           </a>
         </div>
       </Toolbar>
