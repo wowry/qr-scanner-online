f6e7ebe7688cc86776854ccc3b7770e342a7b418
Shuhei Kayawari <dev.wowry@gmail.com>
2022-01-15 00:22:44 +0900

    Add state reset of ImageCardSlice

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,17 @@
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
 import type { NextPage } from "next";
 import Description from "../components/Description";
 import Scanner from "../components/Scanner";
+import { reset } from "../libs/slices/ImageCardSlice";
 
 const Home: NextPage = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(reset());
+  });
+
   return (
     <>
       <Description />
